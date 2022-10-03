f45186f295495481fbe7a1ad3de80ce874461879
chikien111 <38467294+chikien111@users.noreply.github.com>
2022-10-03 16:23:57 +0700

    update new-html

---
+++ script/main.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function ($) {
+ 
+  var App = App || {};
+
+  //---MAIN----
+  $(function () {
+      App.Site.getplugin();
+  });
+
+  //--All site
+    App.Site = function(){
+
+
+        var getplugin = function(){
+
+            // $("#modal-sucess").fancybox({}).trigger('click'); 
+
+
+
+        }
+
+        return{
+            getplugin:getplugin,
+        };
+
+    }(); 
+
+  //--End All site
+
+
+    
+   
+});    
+
+
+
